bb5ce1c364f2a592d8d0d11cc781b0ef5025adbe
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-06 15:56:56 +0545

    Create q1.java

---
+++ Day1/q1.java
@@ -0,0 +1,27 @@
+public class Q1 {
+
+    public String oddString(int n)
+    {
+        String str = "";
+        
+        for(int i=0;i<n;i++)
+        {if(n%2==0&&i==n-1)//if n is even string should be (n-1)number of p and one z = pppz this line will print z at the last iteration only
+            {
+                str+='z';
+            }
+            else
+    str+='p';
+    }
+    return str;
+}
+    
+
+    public static void main(String[] args)
+    {
+        Q1 strgeneratorObj = new Q1();
+        int n=4;
+        System.out.println(strgeneratorObj.oddString(n));
+
+    }
+    
+}
