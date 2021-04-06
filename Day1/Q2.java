9ae2bdfb82563c6e8e47765db497e6c7e6f31621
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-06 23:15:10 +0545

    Add files via upload

---
+++ Day1/Q2.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.util.Stack;
+
+public class Q2 {
+
+    public static void VPS(String inputString)
+    {
+        char poppedchar;
+        Stack stringStack = new Stack();
+        int top=-1;
+        int max_depth = 0, countpushpop=0;
+        for (int i = 0; i < inputString.length(); i++) {
+
+            if ( inputString.charAt(0) == ')' ) 
+            {
+                top = 0;
+                break;
+            }
+            char c = inputString.charAt(i);
+            if (c =='(') 
+            {
+                top++;
+                if (top < 0) {
+                    top = 0;
+                    break;
+                }
+                stringStack.push(c);
+                countpushpop++;
+            }
+            if ( c == ')') {
+                if (top < 0) 
+                {
+                  top = 0;
+                    break;
+                }
+                
+                stringStack.pop();
+                countpushpop--;
+                top--;
+            
+             
+            }
+            max_depth = Math.max(countpushpop,max_depth);
+        }
+       
+        if (top == -1)
+        System.out.println(max_depth);
+        else
+            System.out.println("Invalid VPS");
+            
+    }
+
+
+    
+
+   
+    public static void main(String[] argh) 
+    {
+        
+        Scanner sc = new Scanner(System.in);
+       
+      
+            String inputString = sc.next();
+            Q2.VPS(inputString);
+    }
+            
+}
