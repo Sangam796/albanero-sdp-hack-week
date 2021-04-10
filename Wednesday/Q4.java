2af06a6d56a137aa5d14d1e746cb081ee800a52f
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-10 10:17:56 +0545

    Update Q4.java

--- Wednesday/Q4.java
+++ Wednesday/Q4.java
@@ -1,9 +1,6 @@
+//Q4. Takes an input array of numbers and return a new array, sorted in ascending order (smallest to biggest). If the function's argument is null, an empty array, or undefined; return an empty array.
 import java.util.*;
 public class Q4 {
-
-  
-
-
     public ArrayList<Integer> Sort(ArrayList<Integer> arr) //bubble Sort
     {
         for(int i=0;i<arr.size()-1;i++)
