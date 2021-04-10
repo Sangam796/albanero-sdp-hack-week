0b7b6dc49fcb3dea1b6e739855b33d50362d4755
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-10 10:16:06 +0545

    Update Q5.java

--- Wednesday/Q5.java
+++ Wednesday/Q5.java
@@ -1,10 +1,7 @@
+//Q5. Take an input, array of numbers and return only the even values.
 import java.util.*;
 public class Q5 {
-
-  
-
-
-    public ArrayList<Integer> EvenList(ArrayList<Integer> arr) //bubble sort
+    public ArrayList<Integer> EvenList(ArrayList<Integer> arr) 
     {
         for(int i=0;i<arr.size();i++)
         {
