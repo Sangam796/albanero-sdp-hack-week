17edc812c761ec3144ef1892492033fe51636f04
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-10 09:09:37 +0545

    Update Q7.java

--- Day1/Q7.java
+++ Day1/Q7.java
@@ -1,3 +1,5 @@
+//Q7. Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+Return the length of the longest (contiguous) subarray that contains only 1s. 
 import java.util.Scanner;
 public class Q7 {
 
