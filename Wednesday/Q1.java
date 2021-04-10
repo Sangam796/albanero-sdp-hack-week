bccea55150dc010aeaa9cb7f5541b5892238595c
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-10 09:44:16 +0545

    Update Q1.java

--- Wednesday/Q1.java
+++ Wednesday/Q1.java
@@ -1,15 +1,7 @@
 //find and replace pattern
 import java.util.*;
 public class Q1 {
-
-    public static void main(String[] args) {
-
-        String strList[] =  new String[]{"abc","eee","deq","ijk","mee","aqq","dkd","ddd","ccc"};
-        String pattern = "aaa";
-        Q1 matchedList = new Q1();
-        System.out.println(matchedList.replaceMethod(strList, pattern));
-    }
-        public ArrayList<String> replaceMethod(String[] stringList, String pattern)
+     public ArrayList<String> replaceMethod(String[] stringList, String pattern)
         {
             StringBuilder patternPermutation = new StringBuilder();
             char patternArray[] = pattern.toCharArray();
@@ -71,4 +63,13 @@ public class Q1 {
 
         }
     
+
+    public static void main(String[] args) {
+
+        String strList[] =  new String[]{"abc","eee","deq","ijk","mee","aqq","dkd","ddd","ccc"};
+        String pattern = "aaa";
+        Q1 matchedList = new Q1();
+        System.out.println(matchedList.replaceMethod(strList, pattern));
+    }
+       
 }
